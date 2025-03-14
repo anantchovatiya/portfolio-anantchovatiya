6468376d8d98e20a4503fca92b66454e1161c05b
anantchovatiya <147299670+anantchovatiya@users.noreply.github.com>
2025-03-14 16:06:55 +0530

    Update Projects.tsx

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react';
+// import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import Image from 'next/image';
